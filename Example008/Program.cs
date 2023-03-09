75a1ec246bc9d9632d7389e06353909d9027501d
Denis Ananchenko <denis@ananchenko.ru>
2023-03-09 23:31:30 +0700

    add Example008

---
+++ Example008/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 2 || number == -2)
+{
+    Console.WriteLine(number);
+}
+else
+{
+    if (number > 0)
+    {
+        int result = 2;
+        while (result < number)
+        {
+            Console.Write($"{result}; ");
+            result = result + 2;
+        }
+    }
+    else
+    {
+        int b = -2;
+        while (b > number)
+        {
+            Console.Write($"{b}; ");
+            b = b - 2;
+        }
+    }
+}
