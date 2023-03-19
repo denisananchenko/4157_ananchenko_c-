2772b0ad7bd3cad5db18d27cf4a79ba9b3573aaf
Denis Ananchenko <denis@ananchenko.ru>
2023-03-19 17:44:11 +0700

    add Example023

---
+++ Example023/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result != 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число, или ввели 0. Повторите ввод!");
+        }
+    }
+    return result;
+}
+
+int N = GetNumber("Введите число: ");
+int i = 1;
+int j = 1;
+if (N < 0)
+{
+    N = -N;
+}
+else
+{
+    Console.Write("Таблица кубов: ");
+    while (i <= N)
+    {
+        j = i * i * i;
+        Console.Write($"{j} ");
+        i++;
+    }
+}
