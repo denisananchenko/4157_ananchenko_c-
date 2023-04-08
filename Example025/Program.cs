71aeafb94e7d71467f5508c56eaed5917125adde
Denis Ananchenko <denis@ananchenko.ru>
2023-04-08 17:30:57 +0700

    add Example025

---
+++ Example025/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int GetNumberFromConsole(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+
+int number1 = GetNumberFromConsole("Введите первое число: ");
+int number2 = GetNumberFromConsole("Введите второе число: ");
+double power = Math.Pow(number1, number2);
+Console.WriteLine($"{number1} ^ {number2} = {power}");
