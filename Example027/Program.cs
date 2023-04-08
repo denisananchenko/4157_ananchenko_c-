d5eb66b8b19f7e0d9462932b51caa87b0c345b95
Denis Ananchenko <denis@ananchenko.ru>
2023-04-08 17:34:01 +0700

    add Example027

---
+++ Example027/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int GetNumberFromConsole(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+
+    }
+
+    return result;
+}
+int number = GetNumberFromConsole("Введите число: ");
+if (number < 0)
+    number = -number;
+int sum = 0;
+string numberStr = number.ToString();
+for (int i = 0; i < numberStr.Length; i++)
+{
+    sum = sum + (numberStr[i] - '0');
+}
+Console.WriteLine($"Сума цифр числа {number} = {sum}");
